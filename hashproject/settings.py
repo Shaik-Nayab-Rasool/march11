110159f39a5206d6ecdf35114c339e19dd73ff96
Shaik Nayab Rasool <nayabrasool2050@gmail.com>
2026-03-11 17:07:12 +0530

    allowed host added

--- hashproject/settings.py
+++ hashproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['march11.onrender.com','localhost']
 
 
 # Application definition
